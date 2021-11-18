c03587b71d76bfdff55fed6ed6af9f30faf1779e
FSitnikov1 <mr.prizrek@mail.ru>
2021-11-18 15:54:37 +0300

    actions

--- js/main.js
+++ js/main.js
@@ -1,18 +1,72 @@
-window.addEventListener('DOMContentLoaded', function (event) {
-    console.log("DOM fully loaded and parsed");
-    document.getElementById("btn_call").addEventListener("click", PopUpShow);
-    document.getElementById("btn_uncall").addEventListener("click", PopUpHide);
-    document.getElementById("form_confirm").addEventListener("click", PopUpHide);
+document.addEventListener('DOMContentLoaded', function () {
+
+    console.log('DOM loaded');
+
+    const form = document.getElementById('forma');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+        let error = formValidate(form);
+        let formData = new FormData(form);
+
+        if (error === 0) {
+            form.classList.add('sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+            } else {
+                alert("Ошибка");
+            }
+        } else {
+            alert('Fill the fields!');
+        }
+
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('.req');
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+            if (input.classList.contains('email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        console.log('Error ' + error);
+        return error;
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('error');
+        input.classList.add('error');
+    }
+
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('error');
+        input.classList.remove('error');
+    }
+
+    function emailTest(input) {
+        return !/^\2+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+
 });
-
-//Скрыть PopUp при загрузке страницы  
-window.onload = PopUpHide;
-
-//Функция отображения PopUp
-function PopUpShow() {
-    $(".form_popup").show();
-}
-//Функция скрытия PopUp
-function PopUpHide() {
-    $(".form_popup").hide();
-}
